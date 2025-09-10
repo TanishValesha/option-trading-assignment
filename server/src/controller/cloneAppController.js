105bafd9ab6ce7f35cebbcfc12495f1dad7f448e
Tanish Valesha <tanishsvalesha@gmail.com>
2025-09-10 19:30:09 +0530

    Added chartData fetching function for clonePage

--- server/src/controller/cloneAppController.js
+++ server/src/controller/cloneAppController.js
@@ -5,16 +5,72 @@ const getStockData = async (req, res) => {
 
   try {
     
-    let query = `
+    let infoQuery = `
     SELECT *
     FROM us_clone_warren
     WHERE ticker=$1
     `;
     
-    const { rows } = await pool.query(query, [stock]);
+    const { rows: rows1 } = await pool.query(infoQuery, [stock]);
+
+     let chartQuery = `
+    SELECT 
+    id,
+    stock_ticker,
+    report_date::date AS report_date,
+    revenue,
+    revenue_growth_yoy,
+    cost_of_revenue,
+    gross_profit,
+    selling_general_admin,
+    other_operating_expenses,
+    operating_expenses,
+    operating_income,
+    interest_income,
+    interest_expense,
+    other_expense_income,
+    pretax_income,
+    income_tax,
+    net_income,
+    net_income_growth,
+    shares_outstanding_basic,
+    shares_outstanding_diluted,
+    shares_change,
+    eps_basic,
+    eps_diluted,
+    eps_growth,
+    gross_margin,
+    operating_margin,
+    profit_margin,
+    effective_tax_rate,
+    ebitda,
+    ebitda_margin,
+    ebit,
+    ebit_margin
+FROM ind_fin_data_income
+WHERE stock_ticker = $1;
+
+
+    `;
+    
+    const { rows: rows2 } = await pool.query(chartQuery, [stock]);
+
+   const formattedRows = rows2.map(r => {
+  const d = new Date(r.report_date);
+  // Use local date components
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, "0");
+  const day = String(d.getDate()).padStart(2, "0");
+
+  return {
+    ...r,
+    report_date: `${year}-${month}-${day}`
+  };
+});
 
     res.status(200).json({
-      data: rows,
+      data: rows1,
+      chartData: formattedRows
     });
   } catch (error) {
     console.error("Error fetching stock:", error);
